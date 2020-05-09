70f86c185b1693594ec3fb2f05cabf926e80cb28
Mandy Arens <40013860+maanderz@users.noreply.github.com>
2020-05-09 16:43:38 -0600

    displays cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./styles.css";
+import { Hand } from "react-deck-o-cards";
 
 export default function App() {
+  // return (
+  //   <div className="App">
+  //     <h1>Hello CodeSandbox</h1>
+  //     <h2>Start editing to see some magic happen!</h2>
+  //   </div>
+  // );
+  const defHandStyle = {
+    maxHeight: "34vh",
+    minHeight: "34vh",
+
+    maxWidth: "100vw",
+    padding: 0
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Hand cards={[{ rank: 2, suit: 0 }]} hidden={false} style={defHandStyle} />
   );
 }
